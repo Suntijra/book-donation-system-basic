df9ce389df51cbf09dd9039de7a5c52f22873e7b
Sunti <saknsaza87@gmail.com>
2023-04-02 15:40:07 +0700

    auth admin suceess

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+const secret_key = 'book'
 let AdminController = require('../controllers/adminController')
-let auth = require('../middleware/auth')
-router.post('/', auth, AdminController.loginAdmin)
+function jwt_decoded(token) {
+    return jwt.verify(token, secret_key);
+}
+router.post('/', AdminController.loginAdmin)
+router.get('/protected', (req, res) => {
+    console.log('admin protected')
+    // Get the token from the request headers
+    const token = req.headers.authorization.split(' ')[1];
+    // console.log('token header ',token)
+    try {
+        let decode = jwt_decoded(token)
+        if (decode.level === "admin") {
+            res.status(200).json({ message: 'good' })
+        } else {
+            res.status(200).json({ message: 'bad' })
+        }
+
+    } catch (err) {
+        console.log(err)
+        res.status(200).json({ message: 'bad' })
+    }
+});
 
 module.exports = router
