7a33800d18cfbb01b610e490603c0cee050f34e6
Sunti <saknsaza87@gmail.com>
2023-04-17 01:04:51 +0700

    Update db_mysql.js

--- configs/db_mysql.js
+++ configs/db_mysql.js
@@ -4,6 +4,7 @@ const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 const pool = {
     host: 'localhost',
     user: 'root',
+    password: 'Saknsaza123=',
     database: 'book_donation_center',
 }
 
