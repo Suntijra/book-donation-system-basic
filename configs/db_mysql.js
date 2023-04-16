ce873b8161f616fbc1a1f72f5af92b92f0158a33
Sunti <saknsaza87@gmail.com>
2023-04-17 01:34:54 +0700

    Update db_mysql.js

--- configs/db_mysql.js
+++ configs/db_mysql.js
@@ -4,8 +4,8 @@
 const pool = {
     host: '127.0.0.1',
     user: 'root',
-    password: 'Saknsaza123=',
-    database: 'book_donation_center',
+    password: 'root',
+    database: 'book_donation_center'
 }
 
 module.exports = pool;
