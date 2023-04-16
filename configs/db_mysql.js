788aab1bab8acc51b8777409ccde074a5e2ce83b
Sunti <saknsaza87@gmail.com>
2023-04-17 01:09:50 +0700

    Update db_mysql.js

--- configs/db_mysql.js
+++ configs/db_mysql.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+// require('dotenv').config();
+// const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const pool = {
     host: 'localhost',
